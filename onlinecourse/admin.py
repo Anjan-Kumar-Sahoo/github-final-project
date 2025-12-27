394e75e995af3263c35da14c7e17561d0ba42ee1
Anjan-Kumar-Sahoo <anjankumarsahoo1097@gmail.com>
2025-12-27 22:27:47 +0530

    Add Django settings, templates, and migrations for complete working app

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
 
+# LessonInline class (must be defined before CourseAdmin)
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+
+
 # QuestionInline class
 class QuestionInline(admin.StackedInline):
     model = Question
@@ -14,12 +20,12 @@ class ChoiceInline(admin.StackedInline):
     extra = 4
 
 
-# QuestionAdmin class
-class QuestionAdmin(admin.ModelAdmin):
-    inlines = [ChoiceInline]
-    list_display = ['question_text', 'course', 'lesson', 'grade']
-    list_filter = ['course', 'lesson']
-    search_fields = ['question_text']
+# CourseAdmin class
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]
+    list_display = ('name', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['name', 'description']
 
 
 # LessonAdmin class
@@ -30,17 +36,12 @@ class LessonAdmin(admin.ModelAdmin):
     search_fields = ['title']
 
 
-# CourseAdmin class
-class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
-    list_display = ('name', 'pub_date')
-    list_filter = ['pub_date']
-    search_fields = ['name', 'description']
-
-
-class LessonInline(admin.StackedInline):
-    model = Lesson
-    extra = 5
+# QuestionAdmin class
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['question_text', 'course', 'lesson', 'grade']
+    list_filter = ['course', 'lesson']
+    search_fields = ['question_text']
 
 
 # Register your models here.
